700b1c2b405443a4eeeed876500b94a19dcd1008
Jadson Ricardo <89054834+JadsonRicardo@users.noreply.github.com>
2022-07-16 17:10:07 -0300

    Add files via upload

---
+++ cotação.py
@@ -0,0 +1,10 @@
+import requests 
+url = 'https://economia.awesomeapi.com.br/all/USD-BRL'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    dolar_value = response.json()['USD']['low']
+    print (f'O valor do Dólar é R${dolar_value}')
+else:
+    print('Erro ao buscar o valor do Dólar!')
